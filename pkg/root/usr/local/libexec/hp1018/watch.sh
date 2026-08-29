e732f1f8958e82e6dc6ca3ee47800bdf8ddf6e20
Koen de Boeve <itsmekeuh@gmail.com>
2026-08-29 14:10:21 +0200

    Firmware gets loaded when printer is removed ( usb disconnect/printer power cycle)

--- pkg/root/usr/local/libexec/hp1018/watch.sh
+++ pkg/root/usr/local/libexec/hp1018/watch.sh
@@ -1,19 +1,39 @@
+```bash
 #!/bin/bash
 
-DEVICE_URI='usb://Hewlett-Packard/HP%20LaserJet%201018?serial=KP1N5K4'
 FIRMWARE='/usr/local/share/hp1018/sihp1018.dl'
-STATE='/tmp/hp1018-firmware-loaded'
 
-if /usr/libexec/cups/backend/usb | grep -Fq "$DEVICE_URI"; then
-    if [ ! -f "$STATE" ]; then
-        DEVICE_URI="$DEVICE_URI" \
-        /usr/libexec/cups/backend/usb \
-        999 root HP1018-firmware 1 "" "$FIRMWARE"
+firmware_loaded=0
 
-        if [ $? -eq 0 ]; then
-            touch "$STATE"
+get_device_uri() {
+    /usr/libexec/cups/backend/usb |
+        awk '$1 == "direct" && $2 ~ /^usb:\/\/Hewlett-Packard\/HP%20LaserJet%201018/ {
+            print $2
+            exit
+        }'
+}
+
+while true; do
+
+    DEVICE_URI="$(get_device_uri)"
+
+    if [ -n "$DEVICE_URI" ]; then
+
+        if [ "$firmware_loaded" -eq 0 ]; then
+
+            if DEVICE_URI="$DEVICE_URI" \
+                /usr/libexec/cups/backend/usb \
+                999 root HP1018-firmware 1 "" "$FIRMWARE"
+            then
+                firmware_loaded=1
+            fi
+
         fi
+
+    else
+        firmware_loaded=0
     fi
-else
-    rm -f "$STATE"
-fi
+
+    sleep 10
+done
+```
